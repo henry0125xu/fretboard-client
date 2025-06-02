1a2852f754df5bb5610bda061b7c56008c61e696
henry_ding <henry_ding@alifecom.com>
2025-06-02 17:32:56 +0800

    add pages

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,12 @@ const Contact = () => {
   useEffect(() => {
     document.title = "Contact";
   }, []);
-  return <h1>Contact Page</h1>;
+  return (
+    <>
+      <h1>Contact Page</h1>
+      <p>You can reach us at contact@example.com</p>
+    </>
+  );
 };
 
 export default Contact;
